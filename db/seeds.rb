6de852e3fd83476fbf47ea3ca046b43c31fe0c8e
shinya <shinshin930604@gmail.com>
2016-07-26 13:42:21 +0000

    シードデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+    rand_id = 0
+    until User.find_by_id(rand_id) != nil do
+        rand_id = rand(1..150)
+    end
+    title = "タイトル#{n+1}"
+    content = "内容#{n+1}"
+Blog.create!(title: title, content: content, user_id: rand_id)
+end
