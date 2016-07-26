e70c5115d2622f7affc050c8298ba10df57b0703
shinya <shinshin930604@gmail.com>
2016-07-26 06:38:32 +0000

    ログインメール認証

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+  
+  config.action_mailer.default_url_options = { :host => "damp-tor-32180.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :user_name => "iwashin93@gmail.com",
+    :password =>  "lvyfbbjhhvvedhbb"
+  }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
