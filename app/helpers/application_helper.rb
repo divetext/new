1fd9662a01685d130d05068991ed386a5fe1889d
shinya <shinshin930604@gmail.com>
2016-08-02 12:04:03 +0000

    helper編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,15 @@
 module ApplicationHelper
   def profile_img(user)
-    if user.avatar?
-     return image_tag(user.avatar)
-    else
-      return image_tag('no-image.jpg')
-    end
-    
-    #SNSでログインした時
-    unless user.image_url.blank?
+    return image_tag(user.avatar, alt: user.name) if user.avatar?
+
+    unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no-image.jpg'
     end
-    image_tag(img_url)
+    image_tag(img_url, alt: user.name)
   end
 end
-
-
-
 
 
   
